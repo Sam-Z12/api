8b0c5633381d611c63204d03661a2004bc5c2fc1
DrrZzzs12 <82553480+DrrZzzs12@users.noreply.github.com>
2022-01-12 22:30:44 -0600

    Added pytesting examples

--- tests/calcs_test.py
+++ tests/calcs_test.py
@@ -1,6 +1,17 @@
-from app.calcs import add
+import pytest
+from app.calcs import add, div
+# Run cmd: pytest -v -s in terminal from root directory
+# -v for verbose, -s for showing print statements.
 
 
-def test_add():
-    sum = add(5, 3)
-    assert sum == 8
+@pytest.mark.parametrize("num1, num2, expected", [(5, 5, 10), (3, 3, 6), (7, 2, 9)])
+def test_add(num1, num2, expected):
+    sum = add(num1, num2)
+    assert sum == expected
+
+
+@pytest.mark.parametrize("num1, num2, expected", [(10, 0, 1)])
+def test_div(num1, num2, expected):
+    with pytest.raises(ZeroDivisionError):
+        answer = div(num1, num2)
+        assert answer == expected
